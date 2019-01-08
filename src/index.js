79a66f601ec1446b8a001cca35814edd7ee445e2
dangvanduc90 <ducdangvan@vccorp.vn>
2019-01-08 23:54:55 +0700

    restructure

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// Creates an Express application using the top-level function
 const app = express();
-const jwt = require('jsonwebtoken');
-var sessionMW = require('./common/session');
 var compression = require('compression')
-var cert = require('fs').readFileSync(__dirname + '/../key/jwtRS256.key');
+import routes from './routes'
 
 // Define port number as 3000
 const port = process.env.PORT || 3000;
@@ -24,33 +21,11 @@ app.use(bodyParser.json())
 // Parses the body for POST, PUT, DELETE, etc.
 app.use(bodyParser.urlencoded({ extended: true }));
 
-console.log(app.get('env'))
+// console.log(app.get('env'))
 
 // compress all responses
 app.use(compression())
-
-// specifix middleware
-app.post('/login', sessionMW);
-app.post('/create-user', function(req, res) {
-    let user = {
-        'username': 'dangvanduc90',
-        'email': 'dangvanduc0@gmail.com',
-    }
-    var token = jwt.sign(user, cert, { algorithm: 'RS256' });
-    res.send(token);
-});
-
-// Routes HTTP GET requests to the specified path "/" with the specified callback function
-app.get('/', function(req, res) {
-    res.send('Hello, World!');
-});
-app.post('/', function(req, res) {
-    res.send('Hello, Post Method!');
-});
- 
-app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-})
+app.use(routes)
 
 // Make the app listen on port 3000
 app.listen(port, function() {
---
+++ src/routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+var sessionMW = require('./../common/session');
+const jwt = require('jsonwebtoken');
+var sessionMW = require('./../common/session');
+var cert = require('fs').readFileSync(__dirname + '/../../key/jwtRS256.key');
+
+router.get('/', function(req, res) {
+    res.send('Hello, World!');
+})
+
+router.get('/users', function(req, res) {
+    res.send('Hello users!');
+})
+
+// specifix middleware
+router.post('/login', sessionMW);
+router.post('/create-user', function(req, res) {
+    let user = {
+        'username': 'dangvanduc90',
+        'email': 'dangvanduc0@gmail.com',
+    }
+    var token = jwt.sign(user, cert, { algorithm: 'RS256' });
+    res.send(token);
+});
+router.post('/', function(req, res) {
+    res.send('Hello, Post Method!');
+});
+router.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+module.exports = router
