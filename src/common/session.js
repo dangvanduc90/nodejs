491f2f10c63df976a939d99a99e2556d874e4370
Đặng Văn Đức <ducdangvan@vccorp.vn>
2019-01-08 16:30:04 +0700

    add specifix middleware, jwt, static file, body parse

--- src/common/session.js
+++ src/common/session.js
@@ -1,4 +1,17 @@
 var jwt = require('jsonwebtoken');
+var pubkey = require('fs').readFileSync(__dirname + '/../../key/jwtRS256.key.pub');
+
 module.exports = function(req,res,next){
+    let token = req.headers.authorization || req.headers['x-access-token'];
+     //console.log(token);
+     jwt.verify(token,pubkey,{algorithms:['RS256']},(err,decoded) => {
+        if (err){
+            res.status(401).json({success:false,message:'Failed to authenticate token.'});
+        }else{
+            req.user = decoded;
+            console.log(decoded)
+            next();
+        }
+    })
     next()
 }
--- src/index.js
+++ src/index.js
@@ -4,18 +4,36 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 // Creates an Express application using the top-level function
 const app = express();
+const jwt = require('jsonwebtoken');
 var sessionMW = require('./common/session');
+
+var cert = require('fs').readFileSync(__dirname + '/../key/jwtRS256.key');
 
 // Define port number as 3000
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-// middleware
-app.use(sessionMW);
+app.use(express.static('public'))
+// common middleware
+// app.use(sessionMW);
 
+// parse application/json
+app.use(bodyParser.json())
+// parse application/x-www-form-urlencoded
 // Parses the body for POST, PUT, DELETE, etc.
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// specifix middleware
+app.post('/login', sessionMW);
+app.post('/create-user', function(req, res) {
+    let user = {
+        'username': 'dangvanduc90',
+        'email': 'dangvanduc0@gmail.com',
+    }
+    var token = jwt.sign(user, cert, { algorithm: 'RS256' });
+    res.send(token);
+});
 
 // Routes HTTP GET requests to the specified path "/" with the specified callback function
 app.get('/', function(req, res) {
@@ -24,6 +42,10 @@ app.get('/', function(req, res) {
 app.post('/', function(req, res) {
     res.send('Hello, Post Method!');
 });
+ 
+app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 
 // Make the app listen on port 3000
 app.listen(port, function() {
